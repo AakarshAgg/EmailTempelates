afbf2a75fab3216469e63286ec8518b509f7c750
Aakarsh <aakarshaggarwal7@gmail.com>
2022-09-27 15:26:40 +0530

    All files are added

---
+++ email.js
@@ -0,0 +1,4 @@
+function myfunction(){
+    return alert("Thanks!!");   //new URL('https://www.atg.world/view-event/42816/pre-internship-talk-session-by-across-the-globe-atg-social-network-for-students')
+    
+}
